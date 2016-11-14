74d5ec0da9d848205f38c087be55df4baa4bd991
hong7463 <hison7463@gmail.com>
2016-11-14 14:48:08 -0800

    merge map view with camera view

--- app/src/main/java/com/codepath/finderapp/activities/MainActivity.java
+++ app/src/main/java/com/codepath/finderapp/activities/MainActivity.java
@@ -2,31 +2,53 @@ package com.codepath.finderapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.codepath.finderapp.DispatchActivity;
 import com.codepath.finderapp.R;
+import com.codepath.finderapp.adapters.HomeViewPagerAdapter;
 import com.codepath.finderapp.fragments.HomeMapView;
+import com.codepath.finderapp.models.PicturePost;
 import com.codepath.finderapp.utils.AppUtils;
 import com.crashlytics.android.Crashlytics;
 import com.facebook.login.LoginManager;
 import com.parse.ParseUser;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.activity_main_view_pager)
+    ViewPager viewPager;
+    @BindView(R.id.activity_main_tab)
+    TabLayout tab;
+
+    private HomeViewPagerAdapter adapter;
+    private PicturePost post;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
-
         setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, CameraPostActivity.class);
-        startActivity(i);
+        Fabric.with(this, new Crashlytics());
+        ButterKnife.bind(this);
+
+        post = new PicturePost();
+
+        adapter = new HomeViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tab.setupWithViewPager(viewPager);
+
+//        setContentView(R.layout.activity_main);
+//        Intent i = new Intent(this, CameraPostActivity.class);
+//        startActivity(i);
 
         // New test creation of object below
         //ParseObject testObject = new ParseObject("TestObject");
@@ -65,38 +87,11 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DispatchActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
-
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
-        return true;
+
+    public PicturePost getCurrentPicturePost() {
+        return post;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-
-            case R.id.logout:
-                onLogout();
-                break;
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void onLogout() {
-        // Log the user out
-        ParseUser.logOut();
-        // close this user's session
-        LoginManager.getInstance().logOut();
-        // Go to the login view
-        Intent intent = new Intent(this, DispatchActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-
 }
 
---
+++ app/src/main/java/com/codepath/finderapp/adapters/HomeViewPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.codepath.finderapp.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.codepath.finderapp.fragments.CameraFragment;
+import com.codepath.finderapp.fragments.HomeMapView;
+
+/**
+ * Created by hison7463 on 11/14/16.
+ */
+
+public class HomeViewPagerAdapter extends FragmentPagerAdapter {
+
+    private static int NUM_ITEMS = 2;
+    private String[] titles = {"MAP", "CAM"};
+
+    public HomeViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: {
+                return new HomeMapView();
+            }
+            case 1: {
+                return new CameraFragment();
+            }
+            default: {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
