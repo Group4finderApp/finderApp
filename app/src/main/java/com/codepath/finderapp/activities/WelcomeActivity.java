b56aa925917824adf3a5e8540cd4d1df0b34a781
Roy Choudhury <manisharc@gmail.com>
2016-12-05 23:16:15 -0800

    Updating the welcome screen to have simple animation

--- app/src/main/java/com/codepath/finderapp/activities/WelcomeActivity.java
+++ app/src/main/java/com/codepath/finderapp/activities/WelcomeActivity.java
@@ -1,5 +1,8 @@
 package com.codepath.finderapp.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
@@ -8,6 +11,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.codepath.finderapp.R;
 import com.codepath.finderapp.network.FacebookGraphClient;
@@ -24,12 +29,14 @@ import java.util.Arrays;
 public class WelcomeActivity extends Activity {
 
     private Dialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         //Uri targetUrl = AppLinks.getTargetUrlFromInboundIntent(this, getIntent());
         // Sign up with facebook
+        playAnimation();
         Button facebookLoginButton = (Button) findViewById(R.id.facebooklogin_button);
         facebookLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,4 +85,148 @@ public class WelcomeActivity extends Activity {
         startActivity(intent);
     }
 
+    public void playAnimation() {
+
+        final ImageView marker1 = (ImageView) findViewById(R.id.marker1);
+        final ImageView marker2 = (ImageView) findViewById(R.id.marker2);
+        final ImageView marker3 = (ImageView) findViewById(R.id.marker3);
+        final ImageView marker4 = (ImageView) findViewById(R.id.marker4);
+        final ImageView marker5 = (ImageView) findViewById(R.id.marker5);
+        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.rl);
+
+        marker1.setVisibility(View.VISIBLE);
+
+        ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(marker1, "alpha", 0f, 1f);
+        fadeAnim.setDuration(2000);
+        fadeAnim.start();
+        fadeAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                marker2.setVisibility(View.VISIBLE);
+                ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(marker2, "alpha", 0f, 1f);
+                fadeAnim.setDuration(2000);
+                fadeAnim.start();
+                fadeAnim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        marker3.setVisibility(View.VISIBLE);
+                        ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(marker3, "alpha", 0f, 1f);
+                        fadeAnim.setDuration(2000);
+                        fadeAnim.start();
+
+                        fadeAnim.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                marker4.setVisibility(View.VISIBLE);
+                                ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(marker4, "alpha", 0f, 1f);
+                                fadeAnim.setDuration(2000);
+                                fadeAnim.start();
+                                fadeAnim.addListener(new AnimatorListenerAdapter() {
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        marker5.setVisibility(View.VISIBLE);
+                                        ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(marker5, "alpha", 0f, 1f);
+                                        fadeAnim.setDuration(2000);
+                                        fadeAnim.start();
+
+                                        fadeAnim.addListener(new AnimatorListenerAdapter() {
+                                            @Override
+                                            public void onAnimationEnd(Animator animation) {
+                                                rl.setVisibility(View.VISIBLE);
+                                                ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(rl, "alpha", 0f, 1f);
+                                                fadeAnim.setDuration(2000);
+                                                fadeAnim.start();
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+
+        /*final ImageView pawPrint1 = (ImageView) findViewById(R.id.pawPrint1);
+        final ImageView pawPrint2 = (ImageView) findViewById(R.id.pawPrint2);
+
+        pawPrint1.setVisibility(View.INVISIBLE);
+        pawPrint1.setVisibility(View.INVISIBLE);
+        ObjectAnimator rotateAnim = ObjectAnimator.ofFloat(pawPrint1, "rotation", 45f);
+        ObjectAnimator rotateAnim1 = ObjectAnimator.ofFloat(pawPrint2, "rotation", 45f);
+
+        AnimatorSet set1 = new AnimatorSet();
+        set1.playTogether(rotateAnim, rotateAnim1);
+        set1.start();
+
+        pawPrint1.setVisibility(View.VISIBLE);
+        pawPrint1.setVisibility(View.VISIBLE);
+
+        ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(pawPrint1, "alpha", 0f);
+        fadeAnim.setDuration(2000);
+        fadeAnim.start();
+        fadeAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                pawPrint1.setVisibility(View.INVISIBLE);
+                ObjectAnimator anim1 = ObjectAnimator.ofFloat(pawPrint1, "translationX", 100f);
+                ObjectAnimator anim2 = ObjectAnimator.ofFloat(pawPrint1, "translationY", -5f);
+                ObjectAnimator anim3 = ObjectAnimator.ofFloat(pawPrint1, "rotation", 40f);
+                AnimatorSet set1 = new AnimatorSet();
+                set1.playTogether(anim1, anim2, anim3);
+                set1.start();
+                set1.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        pawPrint1.setVisibility(View.VISIBLE);
+                        ObjectAnimator anim3 = ObjectAnimator.ofFloat(pawPrint1, "alpha", 0f, 1f);
+                        anim3.start();
+                        anim3.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(pawPrint2, "alpha", 0f);
+                                fadeAnim.setDuration(2000);
+                                fadeAnim.start();
+                                fadeAnim.addListener(new AnimatorListenerAdapter() {
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        pawPrint2.setVisibility(View.INVISIBLE);
+                                        ObjectAnimator anim1 = ObjectAnimator.ofFloat(pawPrint2, "translationX", 100f);
+                                        ObjectAnimator anim2 = ObjectAnimator.ofFloat(pawPrint2, "translationY", -5f);
+                                        ObjectAnimator anim3 = ObjectAnimator.ofFloat(pawPrint2, "rotation", 40f);
+                                        AnimatorSet set1 = new AnimatorSet();
+                                        set1.playTogether(anim1, anim2, anim3);
+                                        set1.start();
+                                        set1.addListener(new AnimatorListenerAdapter() {
+                                            @Override
+                                            public void onAnimationEnd(Animator animation) {
+                                                pawPrint2.setVisibility(View.VISIBLE);
+                                                ObjectAnimator anim3 = ObjectAnimator.ofFloat(pawPrint2, "alpha", 0f, 1f);
+                                                anim3.start();
+
+
+                                            }
+                                        });
+
+                                    }
+                                });
+
+
+                            }
+                        });
+                    }
+
+                    //fadeAnim.start();
+
+
+
+
+                });
+
+
+            }
+        });*/
+    }
 }
+
