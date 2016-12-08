5b4fb780bfb636c577d4c5f3b6008ff7ecf77e0f
hong7463 <hison7463@gmail.com>
2016-12-08 02:36:03 -0800

    solve conflict

--- app/src/main/java/com/codepath/finderapp/fragments/HomeMapFragment.java
+++ app/src/main/java/com/codepath/finderapp/fragments/HomeMapFragment.java
@@ -17,6 +17,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v7.app.AlertDialog;
